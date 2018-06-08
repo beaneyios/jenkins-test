18a3c0999bfe6d3c1cfdd8497eed3820d43fbd79
Matt <beaneyios@gmail.com>
2018-06-08 10:26:00 +0100

    Initial Commit

---
+++ JenkinsTest/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  JenkinsTest
+//
+//  Created by Matthew Beaney on 08/06/2018.
+//  Copyright © 2018 BBC News. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
