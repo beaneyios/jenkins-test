64b7f2588d969d50461455443ade3d669843dde8
Matt <beaneyios@gmail.com>
2018-06-22 13:26:19 +0100

    Removed incorrect syntax.

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -1,6 +1,6 @@
 stage('Setting the variables values') {
     node {
-    	print "${env.Flavour}"]
+    	print "${env.Flavour}"
     	print params.Flavour
          sh '''#!/bin/bash
                 fastlane tests 
