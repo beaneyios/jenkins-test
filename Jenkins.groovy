c4ecaa690e4aa33436a0f7bcd1d851c4eef14c3c
Matt <beaneyios@gmail.com>
2018-06-25 12:42:09 +0100

    Overriding slack settings

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -1,5 +1,5 @@
 stage('Setting the variables values') {
     node {
-         slackSend (color: '#FFFF00', message: "STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})")
+         slackSend (color: '#FFFF00', message: "STARTED: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]' (${env.BUILD_URL})", baseURL: "https://hooks.slack.com/services/T03N363D0/BB6671VMW/", token: "BaePQoyadJ45P5IJJbl8Uu3B", channel: "testjenkins")
     }
 }
