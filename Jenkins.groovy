8fa6f8baa37c9f2dad3a6d039fc657814ff0ba61
Matt <beaneyios@gmail.com>
2018-06-29 09:36:45 +0100

    Quick fix to input variables

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -3,7 +3,7 @@ def notify
 stage('Setting the variables values') {
     node {
     	notify = load '../Child@script/notify.groovy'
-        notifySuccess(params.flavour, stageName)
-        notifyFailure(params.flavour, stageName)
+        notifySuccess("GNL", "Unit tests")
+        notifyFailure("GNL", "Unit tests")
     }
 }
