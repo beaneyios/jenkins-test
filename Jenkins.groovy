2b29e9758b1800b9449363977d71672958518000
Matt <beaneyios@gmail.com>
2018-06-29 09:31:23 +0100

    Slight syntax change

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -2,7 +2,7 @@ def notify
 
 stage('Setting the variables values') {
     node {
-    	notify = load "notify.groovy"
+    	notify = load 'notify.groovy'
         notifySuccess(params.flavour, stageName)
         notifyFailure(params.flavour, stageName)
     }
