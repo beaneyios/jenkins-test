eb9a44988a2ef45c13013ca1a93e678c55f9291b
Matt <beaneyios@gmail.com>
2018-06-29 09:28:47 +0100

    Updated again

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -2,9 +2,8 @@ def notify
 
 stage('Setting the variables values') {
     node {
-    	notify = load "${pwd()}/notify.groovy"
-
-         notifySuccess(params.flavour, stageName)
-         notifyFailure(params.flavour, stageName)
+    	notify = load "notify.groovy"
+        notifySuccess(params.flavour, stageName)
+        notifyFailure(params.flavour, stageName)
     }
 }
