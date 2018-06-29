7aafab854046ecbbaa2ded66097b145f70799def
Matt <beaneyios@gmail.com>
2018-06-29 09:37:25 +0100

    calling notifySuccess on object now

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -3,7 +3,7 @@ def notify
 stage('Setting the variables values') {
     node {
     	notify = load '../Child@script/notify.groovy'
-        notifySuccess("GNL", "Unit tests")
-        notifyFailure("GNL", "Unit tests")
+        notify.notifySuccess("GNL", "Unit tests")
+        notify.notifyFailure("GNL", "Unit tests")
     }
 }
