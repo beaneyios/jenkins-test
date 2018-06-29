afd8af1b6095900274eda9f87f72c717b446ca9a
Matt <beaneyios@gmail.com>
2018-06-29 09:34:49 +0100

    Trying different way of loading script.

--- Jenkins.groovy
+++ Jenkins.groovy
@@ -2,7 +2,7 @@ def notify
 
 stage('Setting the variables values') {
     node {
-    	notify = load 'notify.groovy'
+    	notify = load '../Child@script/notify.groovy'
         notifySuccess(params.flavour, stageName)
         notifyFailure(params.flavour, stageName)
     }
